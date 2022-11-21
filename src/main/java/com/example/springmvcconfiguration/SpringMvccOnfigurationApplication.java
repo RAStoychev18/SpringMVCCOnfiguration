e1911512bfda338069a38b347e50654ef6a027a9
RAStoychev18 <RAStoychev18@codingburgas.bg>
2022-11-21 11:42:11 +0200

    initial commit

---
+++ src/main/java/com/example/springmvcconfiguration/SpringMvccOnfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvcconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvccOnfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvccOnfigurationApplication.class, args);
+    }
+
+}
